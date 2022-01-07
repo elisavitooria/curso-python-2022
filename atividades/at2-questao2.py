c8ebd9009359d19e29aac9c9e3ec82d1a1688a80
elisa vitoria <elisavitoria188@gmail.com>
2022-01-07 18:00:19 -0300

    questao 2

---
+++ atividades/at2-questao2.py
@@ -0,0 +1,11 @@
+#Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+#entrada
+valor = float(input('apresente um número:'))
+
+#saída
+if valor>= 0:
+  print('o número', valor, 'é positivo')
+if valor < 0:
+  print("o número,", valor, "é negativo")
+
+
