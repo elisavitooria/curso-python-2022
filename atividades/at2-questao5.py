342bf71d0ea840d9b114d0fb627f12938dc52e2f
elisa vitoria <elisavitoria188@gmail.com>
2022-01-07 21:43:20 -0300

    mais questões

---
+++ atividades/at2-questao5.py
@@ -0,0 +1,17 @@
+#Faça um Programa que verifique se uma letra digitada é vogal ou consoante
+letra = input("digite uma letra ou consoante:")
+
+if letra =="a":
+    print("Vogal")
+elif letra == "e":
+    print("Vogal")
+elif letra == "i":
+    print("Vogal")
+elif letra == "o":
+    print("Vogal")
+elif letra == "u":
+    print("Vogal")
+    print("Insira uma letra ou consoante")
+else:
+    print("Consoante")
+
